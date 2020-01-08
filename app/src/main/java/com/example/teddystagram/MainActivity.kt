14f9db4228717832aa69f234204d0ae61acc3985
최창익 <aackddlr@gmail.com>
2020-01-08 14:30:29 +0900

    메인엑티비티 실행 버그 수정

--- app/src/main/java/com/example/teddystagram/MainActivity.kt
+++ app/src/main/java/com/example/teddystagram/MainActivity.kt
@@ -75,7 +75,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
         //Set default screen
         bottom_navigation.selectedItemId = R.id.action_home
-        registerPushToken()
+        //registerPushToken()
     }
 
     fun registerPushToken(){
