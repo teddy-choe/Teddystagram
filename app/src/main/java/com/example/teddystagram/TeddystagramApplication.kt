a251b0ce419f5820d552cc67b081bd0e5a2ebb66
Teddy_changik <aackddlr@gmail.com>
2021-07-13 14:44:44 +0900

    refactor : 불필요 코드 제거

--- app/src/main/java/com/example/teddystagram/TeddystagramApplication.kt
+++ app/src/main/java/com/example/teddystagram/TeddystagramApplication.kt
@@ -5,14 +5,7 @@ import android.content.Context
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
 
-//TODO: companionObject로 globalContext를 제공하는 것이 구조적으로 문제 없는지 검토 필요
 class TeddystagramApplication: Application() {
-    companion object {
-        fun getGlobalApplicationContext(): Context {
-            return this.getGlobalApplicationContext()
-        }
-    }
-
     override fun onCreate() {
         super.onCreate()
 
